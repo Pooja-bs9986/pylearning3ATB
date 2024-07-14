f984c72dbf8bb828c22548186e16c8e9652078c8
Pooja bs <poojashetty9986@gmail.com>
2024-07-14 16:15:55 +0530

    task1

--- Lab002.py
+++ Lab002.py
@@ -1,11 +1,12 @@
 # Develop a Python script that calculates the square and cube of a given number. num = 2 sq - 4, c = 8
 import math
+
 print("Welcome")
 num = 2
 sq = num ** 2
 print(f"The square of {num} is {sq}")
 c = num ** 3
-print(f"The cube of {num} is {c}")+
+print(f"The cube of {num} is {c}")
 
 num = 2
 sq = math.pow(num, 2)
